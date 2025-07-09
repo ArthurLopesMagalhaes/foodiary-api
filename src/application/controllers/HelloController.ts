49f1a54618939c52346733aa7a3c1ca5d966ea68
Arthur Lopes <arthur.lopes@sydle.com>
2025-07-08 22:04:03 -0300

    feat: implement helloSchema types

--- src/application/controllers/HelloController.ts
+++ src/application/controllers/HelloController.ts
@@ -1,17 +1,10 @@
-import z from 'zod';
 import { Controller } from '../contracts/Controller';
-
-const schema = z.object({
-  account: z.object({
-    name: z.string().min(1, 'Name is required'),
-  }),
-  email: z.string().min(1, 'Email is required').email('Invalid email'),
-});
+import { HelloBody, helloSchema } from '../schemas/helloSchema';
 
 export class HelloController extends Controller<unknown> {
-  protected override schema = schema;
+  protected override schema = helloSchema;
 
-  protected override async handle(request: Controller.Request): Promise<Controller.Response<unknown>> {
+  protected override async handle(request: Controller.Request<HelloBody>): Promise<Controller.Response<unknown>> {
     return {
       statusCode: 200,
       body: {

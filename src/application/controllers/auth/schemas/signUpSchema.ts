71522c15c22a3f28799642a16c7d0a65344f0e16
Arthur Lopes <arthur.lopes@sydle.com>
2025-07-19 15:22:26 -0300

    feat: implement error handling for authentication; add InvalidCredentials and InvalidRefreshToken errors; update SignInUseCase and RefreshTokenUseCase to throw these errors on failure

--- src/application/controllers/auth/schemas/signUpSchema.ts
+++ src/application/controllers/auth/schemas/signUpSchema.ts
@@ -3,9 +3,7 @@ import z from 'zod';
 export const signUpSchema = z.object({
   account: z.object({
     email: z.string().min(1, '"email" is required').email('Invalid email'),
-    password: z.string()
-      .min(1, '"password" is required')
-      .min(6, '"password" must be at least 6 characters long'),
+    password: z.string().min(8, '"password" must be at least 8 characters long'),
   }),
 });
 
